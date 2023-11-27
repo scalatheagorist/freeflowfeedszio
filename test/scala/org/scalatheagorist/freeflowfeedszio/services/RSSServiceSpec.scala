2d5f3cb2efb4a4054a9de9e45a90919d2611a27c
scalanakamoto <lightningrises@proton.me>
2023-11-27 11:05:56 +0100

    ...

--- test/scala/org/scalatheagorist/freeflowfeedszio/services/RSSServiceSpec.scala
+++ test/scala/org/scalatheagorist/freeflowfeedszio/services/RSSServiceSpec.scala
@@ -12,6 +12,7 @@ import zio.Chunk
 import zio.Scope
 import zio.ZIO
 import zio.ZLayer
+import zio.durationInt
 import zio.http.Client
 import zio.stream.ZStream
 import zio.test.Spec
@@ -19,47 +20,81 @@ import zio.test.TestEnvironment
 import zio.test.ZIOSpecDefault
 import zio.test.assertTrue
 
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneId
+
 object RSSServiceSpec extends ZIOSpecDefault {
-  override def spec: Spec[TestEnvironment with Scope, Any] = suite("RSSService") {
-    test("generateFeeds") {
-      val appConfig         = mock[AppConfig]
-      val fileStoreClient   = mock[FileStoreClient]
-      val rssBuilder        = mock[RSSBuilder]
-      val httpClient        = mock[HttpClient]
-      val htmlScrapeService = mock[HtmlScrapeService]
-      val clock             = mock[java.time.Clock]
+  private val appConfig = mock[AppConfig]
+  private val fileStoreClient = mock[FileStoreClient]
+  private val rssBuilder = mock[RSSBuilder]
+  private val httpClient = mock[HttpClient]
+  private val htmlScrapeService = mock[HtmlScrapeService]
+  private val clock: Clock =
+    Clock.fixed(
+      Instant.parse("2023-11-15T12:00:00.00Z"),
+      ZoneId.of("UTC")
+    )
 
-      val clockLayer = ZLayer(ZIO.succeed(clock))
-      val appConfigLayer = ZLayer(ZIO.succeed(appConfig))
+  private val clockLayer = ZLayer(ZIO.succeed(clock))
+  private val appConfigLayer = ZLayer(ZIO.succeed(appConfig))
+  private val rssBuilderLayer = ZLayer.succeed(rssBuilder)
 
-      val fileStoreConfigLayer = appConfigLayer >>> ZLayer(ZIO.service[AppConfig].map(_.fileStoreConfig))
+  private val fileStoreConfigLayer =
+    appConfigLayer >>> ZLayer(ZIO.service[AppConfig].map(_.fileStoreConfig))
 
-      val clientLayer = ZLayer.suspend(Client.default)
+  private val clientLayer =
+    ZLayer.suspend(Client.default)
 
-      val httpClientLayer = clientLayer >>> ZLayer.succeed(httpClient)
+  private val httpClientLayer =
+    clientLayer >>> ZLayer.succeed(httpClient)
 
-      val fileStoreClientLayer = (appConfigLayer ++ fileStoreConfigLayer) >>> ZLayer.succeed(fileStoreClient)
+  private val fileStoreClientLayer =
+    (appConfigLayer ++ fileStoreConfigLayer) >>> ZLayer.succeed(fileStoreClient)
 
-      val htmlScrapeServiceLayer = (httpClientLayer ++ appConfigLayer ++ fileStoreClientLayer) >>> ZLayer.succeed(htmlScrapeService)
+  private val htmlScrapeServiceLayer =
+    (httpClientLayer ++ appConfigLayer ++ fileStoreClientLayer) >>> ZLayer.succeed(htmlScrapeService)
 
-      val expectedJson = """{"author": "Schweizer Monat", "article": {"title": "\u00abMan findet die Macht  nie dort, wo man sie sucht\u00bb", "link": "https://schweizermonat.ch/man-findet-die-macht-nie-dort-wo-man-sie-sucht/"}, "publisher": "SCHWEIZER_MONAT", "lang": "DE"}""".stripMargin
 
-      (for {
-        _        <- ZIO.succeed(fileStoreClient.loadFromDir returns ZStream(Chunk.fromArray(expectedJson.getBytes("UTF-8"))))
-        _        <- ZIO.succeed(rssBuilder.build(Some(Publisher.SCHWEIZERMONAT), None)(*) returns ZStream("<div>test</div>"))
+  override def spec = suite("RSSService")(
+    suite("generateFeeds") {
+      test("must work") {
+        val expectedJson =
+          """{"author": "Schweizer Monat", "article": {"title": "\u00abMan findet die Macht  nie dort, wo man sie sucht\u00bb", "link": "https://schweizermonat.ch/man-findet-die-macht-nie-dort-wo-man-sie-sucht/"}, "publisher": "SCHWEIZER_MONAT", "lang": "DE"}""".stripMargin
 
-        charSeq  <- ZIO.serviceWithZIO[RSSService] { service =>
-                   service.generateFeeds(Some(Publisher.SCHWEIZERMONAT), None).runLast
-                 }
-      } yield assertTrue(charSeq.contains("<div>test</div>")))
-        .provideLayer(
-          (clockLayer ++
-            ZLayer.succeed(rssBuilder) ++
-            appConfigLayer ++
+        // mock
+        fileStoreClient.loadFromDir returns ZStream(Chunk.fromArray(expectedJson.getBytes("UTF-8")))
+        rssBuilder.build(Some(Publisher.SCHWEIZERMONAT), None)(*) returns ZStream("<div>test</div>")
+
+        // act & assert
+        ZIO
+          .serviceWithZIO[RSSService](_.generateFeeds(Some(Publisher.SCHWEIZERMONAT), None).runLast)
+          .map(charSeq => assertTrue(charSeq.contains("<div>test</div>")))
+          .provideLayer((
+            clockLayer ++
+              rssBuilderLayer ++
+              appConfigLayer ++
+              fileStoreClientLayer ++
+              htmlScrapeServiceLayer
+            ) >>> RSSService.layer)
+      }
+    },
+    suite("runScraper") {
+      test("must work") {
+        htmlScrapeService.stream returns ZStream.unit.provideLayer(clientLayer)
+
+        ZIO.serviceWithZIO[RSSService] { service =>
+          (for {
+            fiber     <- service.runScraper.forkDaemon
+            isRunning <- fiber.status.map(_.isRunning)
+          } yield assertTrue(isRunning)).provideLayer(clientLayer)
+        }.provideLayer((
+          clockLayer ++
+            rssBuilderLayer ++
             fileStoreClientLayer ++
-            htmlScrapeServiceLayer
-          ) >>> RSSService.layer
-        )
-    }
-  }
+            htmlScrapeServiceLayer ++
+            appConfigLayer
+          ) >>> RSSService.layer)
+      }
+    })
 }
